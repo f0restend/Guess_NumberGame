2166165b454240601254f844f7d8d95be5d1fdcb
IT Serega <deluca_IT@protonmail.com>
2022-07-05 19:18:14 +0300

    update GuessNumber

--- src/Guess/GuessNumber.java
+++ src/Guess/GuessNumber.java
@@ -1,31 +1,60 @@
 package Guess;
 import java.util.Scanner;
 
-public class Number {
-    public static void main(String [] args) {
-    }
+public class GuessNumber {
 
     public static void guessingNumberGame() {
 
         Scanner sc = new Scanner(System.in);
-        String number = sc.nextLine();
-        sc.close();
-        System.out.println("Привет, я загадал число от 1 до 5, попробуй угадать. У тебя 5 попыток :)");
 
 
-        switch (number) {
-            case "Нет":
-                System.out.println("В другой раз повезет)");
+        int number = 1 + (int) (10 * Math.random());
 
+        int k = 5;
+
+        int i, guess;
+
+        System.out.println("Привет, выберите число от 1 до 10. " + "У тебя есть 5 попыток чтобы угадать");
+
+        for (i = 0; i < k; i++) {
+
+            System.out.println("Угадайте число");
+
+            guess = sc.nextInt();
+            //sc.close();
+
+            if (number == guess) {
+                System.out.println("Поздравляем! " + "Вы угадали число");
+                break;
+            } else if (number > guess && i != k - 1) {
+                System.out.println("Число больше чем " + guess);
+            } else if (number < guess && i != k - 1) {
+                System.out.println("Число меньше чем " + guess);
+            }
         }
 
-        int i = 1;
-        int b = 5;
+        if (i == k) {
+            System.out.println("Вы использовали все попытки. " + "Попробуйте еще раз :)");
 
-        for (i=0; i< b; i++ ) {
-
-
+            System.out.println("Номер был " + number);
+            sc.close();
         }
     }
 
+    public static void main(String[] args) {
+
+        guessingNumberGame();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
